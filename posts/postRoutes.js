f7e0c9e564e22121e75f5766a1656fee94d0b3d9
Dora Belme <contact.dorabelme@gmail.com>
2019-09-03 17:00:13 -0400

    added get requests

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -6,14 +6,43 @@ const router = express.Router();
 
 // GET posts
 router.get('/', (req, res) => {
-    
     db.find()
         .then(post => {
             res.status(200).json(post);
         })
         .catch(err => {
             res.status(500).json({ error: "The posts information could not be retrieved." });
+        });
+});
+
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+    db.findById(postId)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
         })
+        .catch(err => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        });
+});
+
+router.get('/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    db.findCommentById(postId)
+        .then(comments => {
+            if (comments) {
+                res.status(200).json(comments);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The comments information could not be retrieved." });
+        });
 });
 
 
