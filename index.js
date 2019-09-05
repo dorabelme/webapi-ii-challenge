4c5c79f048fc33349d7dd23f520c8bb3a947dc46
Dora Belme <contact.dorabelme@gmail.com>
2019-09-05 14:49:08 -0400

    changed knewxfile

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
 const server = require('./server.js');
 
-server.listen(8000, () => console.log('API running on port 8000'))
+const port = process.env.PORT || 5000;
+
+
+server.listen(port, () => console.log(`API running on port ${port}`));
